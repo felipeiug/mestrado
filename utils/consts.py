f07b08e90e85904f914256b4914944b4dd2bc255
felipeiug <felipeiug@hotmail.com>
2025-09-02 00:34:27 -0300

    feat: 🎸 Dados para rodar o a MLP

--- utils/consts.py
+++ utils/consts.py
@@ -1,3 +1,61 @@
+USO_SOLO_CLASS = {
+    0: {
+        "name": "No Data",
+        "cor": "#000000",
+    },
+    1: {
+        "name":"Canga",
+        "cor": "#becd47",
+    },
+    2: {
+        "name":"Vegetação Rasteira",
+        "cor": "#9bec9c",
+    },
+    3: {
+        "name":"Industrial",
+        "cor": "#8c1727",
+    },
+    4: {
+        "name":"Residencial",
+        "cor": "#d35625",
+    },
+    5: {
+        "name":"Vegetação Densa",
+        "cor": "#236628",
+    },
+    6: {
+        "name":"Solo Exposto",
+        "cor": "#fff500",
+    },
+    7: {
+        "name":"Água",
+        "cor": "#1a98ff",
+    },
+    8: {
+        "name":"Pousio",
+        "cor": "#ec8de7",
+    },
+    9: {
+        "name":"Agricultura",
+        "cor": "#d009cd",
+    },
+    10: {
+        "name":"10",
+        "cor": "#4c4c4c",
+    },
+    11: {
+        "name":"11",
+        "cor": "#7a7a7a",
+    },
+    12: {
+        "name":"12",
+        "cor": "#a0a0a0",
+    },
+    13: {
+        "name":"13",
+        "cor": "#b7b7b7",
+    },
+}
 
 COLUMNS_INFILTRATION = [
     "Ponto", "Lat", "Lon", "Alt", "00_00", "00_30", "01_00", "01_30", "02_00", "02_30", "03_00", "03_30", "04_00",
