23064916b86aea1227a2f45fb3cbd521256c40b3
felipeiug <felipeiug@hotmail.com>
2025-09-07 12:25:11 -0300

    feat: 🎸 Krigagem para a textura do solo

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pandas as pd
-from utils import Infiltrometro
+from utils import Infiltrometro, plot_soil_texture_classes
 import matplotlib.pyplot as plt
 
-infil = pd.read_excel(r"D:\Mestrado\Trabalho Final\Dados\Levantamento em Campo\Compiled.xlsx", sheet_name="Infiltracao")
-infil = Infiltrometro(infil)
+# infil = pd.read_excel(r"D:\Mestrado\Trabalho Final\Dados\Levantamento em Campo\Compiled.xlsx", sheet_name="Infiltracao")
+# infil = Infiltrometro(infil)
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='ternary')
+plot_soil_texture_classes(ax)
 
-infil.plot_soil_texture(fig, ax)
+# infil.plot_soil_texture(fig, ax)
 
 plt.show()
